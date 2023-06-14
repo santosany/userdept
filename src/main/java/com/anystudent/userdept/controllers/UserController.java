70a4449f53aecb3022d6abcfb91fd67e16fc1309
santosany <any.tecnologia@gmail.com>
2023-06-14 16:30:49 -0300

    annotation Cors

--- src/main/java/com/anystudent/userdept/controllers/UserController.java
+++ src/main/java/com/anystudent/userdept/controllers/UserController.java
@@ -12,12 +12,14 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin(value = "*")
 @RestController
 @RequestMapping(value = "/users")
 public class UserController {
     @Autowired
     private UserService userService;
 
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @ResponseBody
     @GetMapping
     public ResponseEntity<?> userFindAll(){
@@ -25,23 +27,27 @@ public class UserController {
         return new ResponseEntity<>(userList, HttpStatus.OK);
     }
 
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @GetMapping(value = "/{id}", produces = "application/json")
     public ResponseEntity<Optional<User>> findById(@PathVariable Long id){
         Optional<User> userById = userService.userFindById(id);
         return new ResponseEntity<>(userById,HttpStatus.OK);
     }
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @PostMapping
     public ResponseEntity<User> newUser(@RequestBody User newUser){
         User user = userService.mergeUser(newUser);
         return new ResponseEntity<>(user,HttpStatus.CREATED);
     }
 
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @PutMapping(value = "/{id}", produces = "application/json")
     public ResponseEntity<User> updateUser(@RequestBody User putUser){
         User user = userService.mergeUser(putUser);
         return new ResponseEntity<>(user,HttpStatus.OK);
     }
 
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @DeleteMapping(value = "/{id}", produces = "application/json")
     public ResponseEntity<String> deleteUser(@PathVariable Long id){
        userService.deleteUserById(id);
